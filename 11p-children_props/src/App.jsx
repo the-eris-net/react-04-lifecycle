a74bdc3f3396e4609aa033854acea933669837b9
the-eris-net <the.eris.net@gmail.com>
2025-09-19 00:43:22 +0900

    feat: init props

---
+++ 11p-children_props/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          border: "1px solid gray",
+          borderRadius: "8px",
+          padding: "10px",
+        }}
+      >
+        <h2>{this.props.title}</h2>
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Card title="서류 제출 마감 기한">
+          <input type="date" defaultValue="2025-09-15" />
+        </Card>
+        <Card title="오늘 공부 체크리스트">
+          <ul>
+            <li>스터디 준비</li>
+            <li>예제 코드 작성</li>
+          </ul>
+        </Card>
+      </>
+    );
+  }
+}
+
+export default App;
