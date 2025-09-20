08472092ce6bae64f2e09be46acb6d0abb6b6cb2
the-eris-net <the.eris.net@gmail.com>
2025-09-21 01:20:44 +0900

    feat:finish 43p

--- 43p-react_router_render/src/About.jsx
+++ 43p-react_router_render/src/About.jsx
@@ -1,3 +1,14 @@
+import { useState } from 'react';
+
 export default function About() {
-  return <h2>About Page</h2>;
+  const [like, setLike] = useState(0);
+
+  return (
+    <div>
+      <h2>About Page</h2>
+      <button onClick={() => setLike(like + 1)}>
+        자기소개 좋아요 : {like}
+      </button>
+    </div>
+  );
 }
